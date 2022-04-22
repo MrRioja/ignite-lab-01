564ae83becea271b9a0c6e9c8449325988b7e23e
Luiz Rioja <lulyrioja@gmail.com>
2022-04-22 00:29:26 -0300

    fix: fix path of service imports

--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -4,9 +4,9 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 
-import { CoursesService } from 'src/services/courses.service';
-import { EnrollmentsService } from 'src/services/enrollments.service';
-import { StudentsService } from 'src/services/students.service';
+import { CoursesService } from '../services/courses.service';
+import { EnrollmentsService } from '../services/enrollments.service';
+import { StudentsService } from '../services/students.service';
 import { DatabaseModule } from '../database/database.module';
 import { CoursesResolver } from './graphql/resolvers/courses.resolver';
 import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
